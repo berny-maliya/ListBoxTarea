e036a643750ff7f6bcfe9a9a903c26dbc474abd9
berny-maliya <bernardo.urquijo.bu@gmail.com>
2022-02-27 15:52:47 -0700

    Agregar archivos de proyecto.

---
+++ ListBoxTarea/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿
+namespace ListBoxTarea
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lista1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lista2 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lista1
+            // 
+            this.lista1.FormattingEnabled = true;
+            this.lista1.ItemHeight = 16;
+            this.lista1.Location = new System.Drawing.Point(44, 128);
+            this.lista1.Name = "lista1";
+            this.lista1.Size = new System.Drawing.Size(120, 132);
+            this.lista1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(78, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Dato";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(122, 42);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(252, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Añadir";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lista2
+            // 
+            this.lista2.FormattingEnabled = true;
+            this.lista2.ItemHeight = 16;
+            this.lista2.Location = new System.Drawing.Point(301, 128);
+            this.lista2.Name = "lista2";
+            this.lista2.Size = new System.Drawing.Size(120, 132);
+            this.lista2.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(196, 128);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = ">";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(196, 203);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "<";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(196, 157);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = ">>";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(196, 233);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "<<";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lista2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lista1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lista1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox lista2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ ListBoxTarea/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListBoxTarea
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //boton que añade elemento escrito metodo generico
+        public void cambiarUnElemento(ListBox objeto1, ListBox objeto2)
+        {
+            if (objeto1.SelectedIndex != -1)
+            {
+                objeto2.Items.Add((string)objeto1.Items[objeto1.SelectedIndex]);
+                objeto1.Items.RemoveAt(objeto1.SelectedIndex);
+            }
+        }
+        //cambiar todo
+        public void cambiarTodo(ListBox objeto1, ListBox objeto2)
+        {
+           
+            
+                for (int i = objeto1.Items.Count - 1; i >= 0; i--)
+                {
+                    objeto2.Items.Add(objeto1.Items[i]);
+                    objeto1.Items.Remove(objeto1.Items[i]);
+
+                    objeto1.Refresh();
+                    objeto2.Refresh();
+                }
+            
+        }
+        private void button1_Click(object sender, EventArgs e)
+            {
+                lista1.Items.Add(textBox1.Text);
+            }
+            //boton que quita elemento de la primera lista y lo pasa a la otra
+            private void button2_Click(object sender, EventArgs e)
+            {
+           
+            cambiarUnElemento(lista1, lista2);
+
+            }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            cambiarUnElemento(lista2, lista1);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            cambiarTodo(lista1,lista2);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            cambiarTodo(lista2, lista1);
+        }
+    }
+    } 
+
+
